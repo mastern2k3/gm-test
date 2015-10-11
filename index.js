4aa5029f380d855a4543becb04a778476bd8e8c4
mastern2k3 <nitzan.zada@gmail.com>
2015-10-11 15:35:38 +0300

    d

--- index.js
+++ index.js
@@ -3,16 +3,30 @@
 // set PATH=%PATH%;"C:\Program Files\GraphicsMagick-1.3.20-Q8"
 
 var fs = require('fs');
-var gm = require('gm');
+var gm = require('gm'); // .subClass({ imageMagick: true }); // for heroku
+var path = require('path');
+var express = require('express');
 
-// resize and remove EXIF profile data
-gm('C:\\Users\\nitzan.z\\Projects\\gm-test\\512cube.png')
-	.stroke('#003366' , 5)
-	.fill('transparent')
-	.drawPolyline([0, 0], [450, 400], [500, 500])
-	.write('C:\\Users\\nitzan.z\\Projects\\gm-test\\512cube-result.png', function (err) {
-		
-		if (!err) console.log('done');
-		else console.log(err);
-	});
-	
+var app = express();
+
+app.get('/512cube.png', function (req, res, next) {
+
+	gm(path.join(__dirname, '512cube.png'))
+		.stroke('#003366' , 5)
+		.fill('transparent')
+		.drawPolyline([0, 0], [150, 100], [200, 250], [350, 300], [400, 450], [500, 500])
+        .stream(function streamOut (err, stdout, stderr) {
+			
+            if (err) return next(err);
+			
+            res.writeHead(200, { 'Content-Type': 'image/png' }); 
+			
+            stdout.pipe(res);
+
+            stdout.on('error', next);
+    	});
+});
+
+app.listen(3000);
+
+console.log("Listening on port 3000");
